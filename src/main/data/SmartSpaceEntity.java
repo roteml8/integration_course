b13efd697199b3ddf6d46f1505500e358b47913e
Oded <Oded@Oded-Pc>
2019-03-14 21:23:19 +0200

    organized folders

--- src/main/data/SmartSpaceEntity.java
+++
@@ -1,4 +0,0 @@
-public interface SmartSpaceEntity<K> {
-	public K getKey();
-	public void setKey (K key);
-}
--- src/main/java/dao/ActionDao.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface ActionDao {
-public ActionEntity create(ActionEntity actionEntity);
-public List<ActionEntity> readAll(List<ActionEntity>listActionEntity);
-public void deleteAll();
-}
