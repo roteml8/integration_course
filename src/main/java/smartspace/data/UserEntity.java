37f7d67db0eac6643037f43fcbf548b7fcc290cc
גל <@LAPTOP-R465QQ7U>
2019-03-27 23:41:42 +0200

    add userEntity changes

--- src/main/java/smartspace/data/UserEntity.java
+++ src/main/java/smartspace/data/UserEntity.java
@@ -1,7 +1,7 @@
 package smartspace.data;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
+import javax.persistence.Column; 
+ 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
