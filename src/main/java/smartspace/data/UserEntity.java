716bdd9ea71193f25af92c928eb7be0acf0c6251
Oded <Oded@172.50.1.21>
2019-04-03 16:35:14 +0300

    Fixed the user and action tabels.

--- src/main/java/smartspace/data/UserEntity.java
+++ src/main/java/smartspace/data/UserEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="ACTIONS")
+@Table(name="USER")
 public class UserEntity implements SmartspaceEntity<String> {
 
 	private String userSmartspace;
