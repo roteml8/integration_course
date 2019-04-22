9d4208d8b2e5cbaca4214342a00f0703710855ac
Oded <Oded@Oded-Pc>
2019-04-22 19:34:04 +0300

    added ElementService and ElementServiceImpl

---
+++ src/main/java/smartspace/infra/ElementService.java
@@ -0,0 +1,12 @@
+package smartspace.infra;
+
+import java.util.List;
+
+import smartspace.data.ElementEntity;
+
+public interface ElementService {
+	public ElementEntity newElement(ElementEntity entity, int code);
+
+	//TODO return this code when enhanced element entity is done.
+	//public List<ElementEntity> getUsingPagination(int size, int page);
+}
---
+++ src/main/java/smartspace/infra/ElementServiceImpl.java
@@ -0,0 +1,48 @@
+package smartspace.infra;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import smartspace.dao.ElementDao;
+import smartspace.data.ElementEntity;
+
+@Service
+public class ElementServiceImpl implements ElementService {
+	private ElementDao<String> dao;
+	private int CreationCode = 42;
+
+	@Autowired
+	public void setDao(ElementDao<String> dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public ElementEntity newElement(ElementEntity entity, int code) {
+		// validate code
+		if (code != CreationCode) {
+			throw new RuntimeException("you are not allowed to create messages if you don't know the ultimate answer");
+		}
+
+		if (valiadate(entity)) {
+			entity.setCreationTimeDate(new Date());
+			return this.dao.create(entity);
+		} else {
+			throw new RuntimeException("invalid element");
+		}
+	}
+
+	private boolean valiadate(ElementEntity entity) {
+		return entity.getCreatorEmail() != null && entity.getCreatorSmartSpace() != null && entity.getLocation() != null
+				&& entity.getMoreAttributes() != null && entity.getName() != null
+				&& entity.getName().trim().isEmpty() == false && entity.getType() != null
+				&& entity.getType().trim().isEmpty() == false;
+	}
+
+	//TODO return this code when enhanced element entity is done.
+	/*
+	 * @Override public List<ElementEntity> getUsingPagination(int size, int page) {
+	 * return this.dao .readAll("key", size, page); }
+	 */
+}
